c492a374f8b9f4f377448f405228b3d17ce5bf03
Hyunji <rioreo2022@gmail.com>
2022-05-04 16:44:06 +0900

    feat: OrderStatus 추가

--- gc-coffee/src/main/java/com/gccoffee/domain/order/Order.java
+++ gc-coffee/src/main/java/com/gccoffee/domain/order/Order.java
@@ -3,6 +3,7 @@ package com.gccoffee.domain.order;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -10,4 +11,7 @@ import java.util.UUID;
 public class Order {
     private UUID id;
     private Email email;
+    private OrderStatus orderStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
